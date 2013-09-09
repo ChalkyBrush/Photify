d8b5dfe1bc4aa59e0628a341b9557f195dadd34b
rraciop <ryanracioppo@gmail.com>
2013-09-08 22:51:06 -0400

    f

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ Picture.create!(
     :title  => "The old church on the coast of White sea",
     :artist => "Sergey Ershov",
     :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-    :category => "Landscape")
+    :category => "Sports")
 
 Picture.create!(
     :title  => "Sea Power",
@@ -14,4 +14,4 @@ Picture.create!(
     :title  => "Into the Poppies",
     :artist => "John Wilhelm",
     :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg" 
-    :category => "Misc")
+    :category => "Sports")
