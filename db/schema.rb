4d863564f2384e9da9d16dc9269431224b96b9af
rraciop <ryanracioppo@gmail.com>
2013-09-08 22:44:56 -0400

    ohh yes

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130907163749) do
+ActiveRecord::Schema.define(:version => 20130904194815) do
 
   create_table "pictures", :force => true do |t|
     t.string   "artist"
     t.string   "title"
     t.string   "url"
+    t.string   "category"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.string   "category"
   end
 
 end
