02cc807071dbef95b2bc43841d41f5a153cb9e39
rraciop <ryanracioppo@gmail.com>
2013-09-08 21:36:42 -0400

    second attempt

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,16 @@ Picture.create!(
     :title  => "The old church on the coast of White sea",
     :artist => "Sergey Ershov",
     :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-    :category => "Landscape"
-  )
+    :category => "Landscape")
 
 Picture.create!(
     :title  => "Sea Power",
     :artist => "Stephen Scullion",
     :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg" 
-    :category => "Sports" 
-  )
+    :category => "Sports" )
 
 Picture.create!(
     :title  => "Into the Poppies",
     :artist => "John Wilhelm",
     :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg" 
-    :category => "Misc" 
-  )
+    :category => "Misc" )
