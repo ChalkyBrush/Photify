d6ae0f7efb87944dbbda9eb0176749c4f5a086b2
rraciop <ryanracioppo@gmail.com>
2013-09-08 22:54:11 -0400

    testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-Picture.create!(
-    :title  => "The old church on the coast of White sea",
-    :artist => "Sergey Ershov",
-    :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-    :category => "Sports")
+# Picture.create!(
+#     :title  => "The old church on the coast of White sea",
+#     :artist => "Sergey Ershov",
+#     :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+#     :category => "Sports")
 
-Picture.create!(
-    :title  => "Sea Power",
-    :artist => "Stephen Scullion",
-    :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg" 
-    :category => "Sports" )
+# Picture.create!(
+#     :title  => "Sea Power",
+#     :artist => "Stephen Scullion",
+#     :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg" 
+#     :category => "Sports" )
 
-Picture.create!(
-    :title  => "Into the Poppies",
-    :artist => "John Wilhelm",
-    :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg" 
-    :category => "Sports")
+# Picture.create!(
+#     :title  => "Into the Poppies",
+#     :artist => "John Wilhelm",
+#     :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg" 
+#     :category => "Sports")
